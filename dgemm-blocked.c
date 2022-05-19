0886fd2bca65f07d0ea0f6ed0ad830ec3228f0d4
Xiao Song <xiaosx@berkeley.edu>
2022-05-19 00:52:11 -0700

    add todo

--- dgemm-blocked.c
+++ dgemm-blocked.c
@@ -61,6 +61,8 @@ inline void inner_kernel( double* __restrict__ hat_a, \
                           double* __restrict__ hat_c, \
                           int ldc )
 {
+    // TODO: replace this whole function using assembly code. 
+    // Entering asm volatile is expensive
     __m512d R00, R01, R02, R03, R04, R05, R06, R07, R08, R09, \
             R10, R11, R12, R13, R14, R15, R16, R17, R18, R19, \
             R20, R21, R22, R23, R24, R25, R26, R27, R28, R29, \
